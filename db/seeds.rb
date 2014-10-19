8f98a7fe3a9127504b7f1b99b755739b139c0021
Julia López <julialopez@gmail.com>
2014-10-19 11:56:32 +0200

    Seed database with random data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'faker'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+case Rails.env
+when "development"
+  1000.times do |i|
+    Cat.create!(name: Faker::Name.name, birthday: Faker::Date.birthday)
+  end
+end
