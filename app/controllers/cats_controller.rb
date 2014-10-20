ecf9c18e0ac36cb87f001adc41b248c50a82c448
Julia López <julialopez@gmail.com>
2014-10-20 19:14:01 +0200

    Caching #CACHING

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -45,12 +45,17 @@ class CatsController < ApplicationController
   def load_cat_of_the_month
     last_month_follower_relation = FollowerRelation.where("EXTRACT(MONTH FROM created_at) = ? AND EXTRACT(YEAR FROM created_at) = ?", 1.month.ago.month, 1.month.ago.year)
 
-    # First alternative
-    # Retrieve results from database without order and use ruby function to order hash
-    count_of_followers = last_month_follower_relation.group(:followed_cat_id).count
-    # http://www.rubyinside.com/how-to/ruby-sort-hash
-    cat_of_the_month_data = count_of_followers.sort_by { |k, v| -v }.first
-    @cat_of_the_month = Cat.find(cat_of_the_month_data.first) if cat_of_the_month_data
+    current_time       = Time.now
+    date_of_expiration = current_time.end_of_month - current_time
+
+    @cat_of_the_month = Rails.cache.fetch("cat_of_the_month", expires_in: date_of_expiration) do
+      # First alternative
+      # Retrieve results from database without order and use ruby function to order hash
+      count_of_followers = last_month_follower_relation.group(:followed_cat_id).count
+      # http://www.rubyinside.com/how-to/ruby-sort-hash
+      cat_of_the_month_data = count_of_followers.sort_by { |k, v| -v }.first
+      Cat.find(cat_of_the_month_data.first) if cat_of_the_month_data
+    end
 
     # # Second alternative
     # # Order the results with SQL query an retrieve one result
