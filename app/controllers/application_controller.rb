d8daf67a1074308b230fbd72716f1ec7f1d9a8dc
Julia López <julialopez@gmail.com>
2014-10-20 22:16:11 +0200

    Login/Logout functionality #AUTH

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # http://apidock.com/rails/ActionController/Helpers/ClassMethods/helper_method
+  helper_method :loggedin_cat
+  def loggedin_cat
+    loggedin_cat_id = session[:loggedin_cat_id]
+    if loggedin_cat_id
+      Cat.find(loggedin_cat_id)
+    end
+  end
 end
