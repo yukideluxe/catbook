d14946bc76a95822908b5b7266ae5e472792b1a6
Julia Lopez <julia.lopez@xing.com>
2014-10-20 20:45:49 +0200

    has secure password and email presence validation #AUTH

--- spec/factories/cats.rb
+++ spec/factories/cats.rb
@@ -6,6 +6,8 @@ FactoryGirl.define do
   factory :cat do
     name     { Faker::Name.name } # Why using lamdba here?
     birthday { Faker::Date.birthday }
+    email    { Faker::Internet.email }
+    password "aSuPeRsEcUrEpAsSwOrd"
     visible  true
   end
 end
