249c7c0e4f43b929c479c9eef8b8bd98c4780129
Julia López <julialopez@gmail.com>
2014-10-21 23:33:16 +0200

    defined first version of Cats Controller API #API

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
+# http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -11,4 +12,11 @@ Rails.application.routes.draw do
 
   post   "/login",  to: "login#create"
   delete "/logout", to: "login#destroy"
+
+  # API Routes
+  namespace :api do
+    namespace :v1 do
+      resources :cats, only: [:index, :show]
+    end
+  end
 end
